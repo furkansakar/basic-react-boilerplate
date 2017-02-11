4baa566b71be9f4535fcc5bb1642b0a786954bad
Furkan Sakar <Furkan Sakar>
2017-02-11 02:28:30 +0100

    First Commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Component} from 'react';
 
+
 export default class App extends Component{
   render(){
       return (
-          <div>Basic Boilerplate</div>
+          <div>
+              Nothing
+          </div>
       );
   }
 }
